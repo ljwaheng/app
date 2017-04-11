f8a52b264a77d1c421a060696b75f8b2023c8708
ljwaheng <18222965207@163.com>
2017-04-11 09:12:57 +0800

    cache file

--- file.php
+++ file.php
@@ -5,10 +5,11 @@ class File {
 	const EXT = '.txt';
 
 	public function __construct(){
+		//dirname函数用于获取当前文件路径的目录部分
 		$this->_dir=dirname(__FILE__).'/files/';
 	}
 
-	public function cacheData ($key,$value='',$cacheTime=0){
+	public function cacheData($key,$value='',$cacheTime=0){
          $filename=$this->_dir . $key.self::EXT;
 
          if($value !==''){
@@ -16,17 +17,33 @@ class File {
          	if(is_null($value)){
          		return @unlink($filename);
          	}
-         	//将$value值写入缓存
+         	
+			//判断是否存在缓存文件所在目录
          	$dir=dirname($filename);
-         	if(!$dir){
+         	if(!is_dir($dir)){
          		mkdir($dir,0777);
          	}
+			//缓存时间格式为%011d 为%d 时间位数有11位 不够用0填充
+			$cacheTime = sprintf('%011d',$cacheTime);
+			//将$value值和缓存时间写入缓存文件
+			return file_put_contents($filename,$cacheTime.json_encode($value));
          }
 
          //获取缓存文件
          if(!is_file($filename)){
          	return FALSE;
          }
-         $contents =file_get_contents($filename);
+		 $contents =file_get_contents($filename);
+		 $cacheTime = (int)substr($contents,0,11);
+		 //真正的缓存数据
+		 $value=substr($contents,11);
+		 if($cacheTime != 0&&($cacheTime+filemtime($filename)<time())){
+         	   @unlink($filename);			 
+		 }
+		 return json_decode($value,true);         
+		 
 	}
 }
+
+
+
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 	require_once('./response.php');
 	require_once('./db.php');
+	require_once('./file.php');
 	$page = isset($_GET['page']) ? $_GET['page'] : 1;
 	$pagesize = isset($_GET['pagesize']) ? $_GET['pagesize'] : 6;
 	if(!is_numeric($page)||!is_numeric($pagesize)){
@@ -9,19 +10,28 @@
 	}
 
    $offset = ($page-1)*$pagesize;
-   $sql = "select * from test1 limit 0,6";
+   $sql = "select * from test1 limit ".$offset.",".$pagesize;
+   $cache = new File();
+   $results = array();
+   
+   if(!$results=$cache->cacheData('index' . $page .'-' . $pagesize)){
 	  try{ 
 		   $connect =Db::getInstance()->connect();
 	  }catch(Exception $e){	  
 		   return Response::show(403,'失败');
 	  }
-   $res = mysql_query($sql,$connect);
-     
-   $results = array();
-   while($result=mysql_fetch_assoc($res)){
-      $results[]=$result;
-     
-   }
+	   $res = mysql_query($sql,$connect);
+		 
+	   while($result=mysql_fetch_assoc($res)){
+		  $results[]=$result;
+		 
+	   }
+	   
+	   if($results){
+		   $cache->cacheData('index' . $page .'-' . $pagesize,$results,60);
+	   }
+   
+}
    
    if($results){
 	   return Response::show(200,'成功',$results);
